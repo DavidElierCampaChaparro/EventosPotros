be5dbcc1b6970952e9c6cee4ff553f26d204519d
Maximiliano <maximilianoreynaaguilar@gmail.com>
2024-05-09 23:08:10 -0600

    Edición de los diagramas Terminada y Finalización del documento del proyecto

--- src/basesdedatos/Conexion.java
+++ src/basesdedatos/Conexion.java
@@ -14,7 +14,7 @@ public class Conexion {
     String bd = "";
     String url = "jdbc:mysql://localhost:3306/";
     String user = "root";
-    String password = "xr471112";
+    String password = "root";
     String driver = "com.mysql.cj.jdbc.Driver";
     Connection connection;
     
